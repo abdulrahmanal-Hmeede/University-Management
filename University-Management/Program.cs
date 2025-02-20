410f1327c40ab7bfe447a713940b7b79ed8277ee
Faisal Atli <faisalatli020@gmail.com>
2025-02-21 01:19:19 +0300

    test faisal

--- University-Management/Program.cs
+++ University-Management/Program.cs
@@ -7,6 +7,8 @@
             Console.WriteLine("Hello, World!");
             Console.WriteLine("Hello, Team");
             Console.WriteLine("Leader is me");
+
+            Console.WriteLine( "test faisal");
         }
     }
 }

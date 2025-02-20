2e13573f3b7900dfc6991b15f4f227d200d655ec
Abdulrahman Al-Hmeeede <abdulrahmanhmeedi@gmail.com>
2025-02-21 01:00:26 +0300

    init project

---
+++ University-Management/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace University_Management
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

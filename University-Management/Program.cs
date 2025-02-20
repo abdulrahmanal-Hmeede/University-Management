5937338b6cf56106d3cc55e84f90ff2c8e1dd823
Abdulrahman Al-Hmeeede <abdulrahmanhmeedi@gmail.com>
2025-02-21 01:46:17 +0300

    test add lib

--- University-Management/Program.cs
+++ University-Management/Program.cs
@@ -7,7 +7,6 @@
             Console.WriteLine("Hello, World!");
             Console.WriteLine("Hello, Team");
             Console.WriteLine("Leader is me");
-
             Console.WriteLine( "test faisal");
         }
     }
